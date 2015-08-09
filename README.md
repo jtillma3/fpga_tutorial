e8b0f463ec22a61d437ff90c583306efe83d1e0d
Jarius Tillman <jtillma3@gmail.com>
2015-08-08 19:18:29 -0700

    Testing new file structure

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # fpga_tutorial
+* These projects are from the [Embedded Micro Verilog Tutorials](https://embeddedmicro.com/tutorials/mojo)
 
-## Project Structure
-* /ipcore_dir - where IP cores go
-* /iseconfig - used by ISE
-* /src - source folder
-* /syn - working directory for ISE
-* Mojo-Base.xise - the ISE project file
+## Structure
+* Base directory
+    * /ipcore_dir - where IP cores go
+    * /iseconfig - used by ISE
+    * /src - source folder
+    * /syn - working directory for ISE
+    * Mojo-Base.xise - the ISE project file
 
 ## Notes
 * After compilation in the ISE is done, the binary file is saved to the /syn folder.
+
+## Completed Projects
+* Project 1 - Creating a Project
+     * Using the RESET button to control an LED state
---
+++ projects/project1/src/mojo_top.v
@@ -0,0 +1,50 @@
+/*
+This program ties the state of the LED to the state of the RESET button
+*/
+
+module mojo_top(
+    // 50MHz clock input
+    input clk,
+    // Input from reset button (active low)
+    input rst_n,
+    // cclk input from AVR, high when AVR is ready
+    input cclk,
+    // Outputs to the 8 onboard LEDs
+    output[7:0]led,
+    // AVR SPI connections
+    output spi_miso,
+    input spi_ss,
+    input spi_mosi,
+    input spi_sck,
+    // AVR ADC channel select
+    output [3:0] spi_channel,
+    // Serial connections
+    input avr_tx, // AVR Tx => FPGA Rx
+    output avr_rx, // AVR Rx => FPGA Tx
+    input avr_rx_busy // AVR Rx buffer full
+    );
+
+wire rst = ~rst_n; // make reset active high
+wire [9:0] array; // this is a declaration of the 8-bit wire (I/O line)
+// these signals should be high-z when not used
+//append _n to wires that are active-low
+
+//The following three lines are constant declarations
+//x'yz 
+//x - represents the number of bits to use
+//y  - represents how to interpret the written number
+//		possible values: b(inary), he(x)
+//z - represents the value
+//Binary :  1,0, Z,X
+//				Z - High Impedence - disconnected
+//				X - Don't Care - could be anything
+assign spi_miso = 1'bz;   //The spi ports are disconnected to prevent board damage from driving them
+assign avr_rx = 1'bz;
+assign spi_channel = 4'bzzzz;
+
+
+
+assign led[6:0] = 7'b0; //these pins are set to a constant zero
+assign led[7] = rst; // this pin is tied to rst
+
+endmodule
